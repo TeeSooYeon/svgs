6b1db9432d8d76945d46dce14875d413930fefda
TeeSooYeon <60122556+TeeSooYeon@users.noreply.github.com>
2020-10-08 13:38:49 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,150 @@
+<html>
+    <body>
+        <div class="cnl">
+            <h2>Circle and Line</h2>
+                <svg width="200" height="300" style="background-color:#ffff56">
+                    <g>
+                    <line x1="80" y1="0" x2="80" y2="50" stroke-width="2" stroke="#828699"/>
+                    <line x1="100" y1="0" x2="100" y2="100" stroke-width="2" stroke="#828699"/>
+                    <line x1="140" y1="0" x2="140" y2="140" stroke-width="2" stroke="#828699"/>
+                    <line x1="90" y1="300" x2="90" y2="210" stroke-width="2" stroke="#828699"/>
+                    <line x1="120" y1="300" x2="120" y2="180" stroke-width="2" stroke="#828699"/>
+                    <line x1="70" y1="300" x2="70" y2="170" stroke-width="2" stroke="#828699"/>
+                    </g>
+                    <g>
+                    <circle cx="100" cy="100" r="30" fill="#8be8e8" stroke="null" stroke-width="1.5"/>
+                    <circle cx="140" cy="140" r="10" fill="#8be8e8" stroke="null" stroke-width="1.5"/>
+                    <circle cx="120" cy="170" r="20" fill="#8be8e8" stroke="null" stroke-width="1.5"/>
+                    <circle cx="75" cy="160" r="25" fill="#8be8e8" stroke="null" stroke-width="1.5"/>
+                    <circle cx="90" cy="210" r="15" fill="#8be8e8" stroke="null" stroke-width="1.5"/>
+                    <circle cx="80" cy="50" r="15" fill="#8be8e8" stroke="null" stroke-width="1.5"/>
+                    </g>
+                </svg>
+        </div>
+        <div class="cNr">
+            <h2>Circle and Rectangle</h2>
+                <svg width="200" height="300" style="background-color:#828699;">
+                    <g>
+                    <rect x="0" y="50" width="80" height="50" fill="white"/>
+                    <rect x="120" y="100" width="80" height="50" fill="white"/>
+                    <rect x="0" y="150" width="80" height="50" fill="white"/>
+                    <rect x="120" y="200" width="80" height="50" fill="white"/>
+                    </g>
+                    <circle cx="40" cy="25" r="15" fill="#cbe158" stroke="null" stroke-width="1.5"/>
+                    <circle cx="150" cy="75" r="15" fill="#cbe158" stroke="null" stroke-width="1.5"/>
+                    <circle cx="40" cy="125" r="15" fill="#cbe158" stroke="null" stroke-width="1.5"/>
+                    <circle cx="150" cy="175" r="15" fill="#cbe158" stroke="null" stroke-width="1.5"/>
+                    <circle cx="40" cy="225" r="15" fill="#cbe158" stroke="null" stroke-width="1.5"/>
+                    <circle cx="150" cy="275" r="15" fill="#cbe158" stroke="null" stroke-width="1.5"/>
+                </svg>
+        </div>
+        <div class="polyNcircle">
+            <h2>Circle and Rectangle</h2>
+            <svg width="200" height="300" style="background-color:#58bbe1;">
+                <g>
+                <polyline points="0,40 40,40 40,80 80,80 80,120 120,120 120,160 160,160 160,200 200,200 200"
+                style="fill:#58bbe1;stroke:#fdb924;stroke-width:10" />
+                <polyline points="0,180 40,180 40,220 80,220 80,260 120,260 120,300"
+                style="fill:#58bbe1;stroke:#fdb924;stroke-width:10" />
+                </g>
+                <g>
+                  <circle cx="100" cy="40" r="15" fill="#e64980" stroke="null" stroke-width="1.5"/>
+                  <circle cx="165" cy="100" r="15" fill="#e64980" stroke="null" stroke-width="1.5"/>
+                  <circle cx="40" cy="140" r="15" fill="#e64980" stroke="null" stroke-width="1.5"/>
+                  <circle cx="95" cy="185" r="15" fill="#e64980" stroke="null" stroke-width="1.5"/>
+                  <circle cx="140" cy="230" r="15" fill="#e64980" stroke="null" stroke-width="1.5"/>
+                  <circle cx="40" cy="270" r="15" fill="#e64980" stroke="null" stroke-width="1.5"/>
+                </g>
+            </svg>
+        </div>
+        <div class="polygonNellipse">
+            <h2>Polygon and Ellipse</h2>
+            <svg width="200" height="300" style="background-color: #fff68f;">
+                <g>
+                  <polygon points="55,90 95,90 95,30" fill="black" stroke-width="2" stroke="null"/>
+                  <polygon points="140,90 100,90 100,30" fill="black" stroke-width="2" stroke="null"/>
+                  <polygon points="55,95 95,95 95,200" fill="black" stroke-width="2" stroke="null"/>
+                  <polygon points="140,95 100,95 100,200" fill="black" stroke-width="2" stroke="null"/>
+                </g>
+                <g>
+                  <ellipse cx="10" cy="250" rx="50" ry="25" fill="white" stroke-width="1" stroke="null"/>
+                  <ellipse cx="190" cy="250" rx="50" ry="25" fill="white" stroke-width="1" stroke="null"/>
+                  <ellipse cx="190" cy="50" rx="50" ry="25" fill="white" stroke-width="1" stroke="null"/>
+                  <ellipse cx="10" cy="50" rx="50" ry="25" fill="white" stroke-width="1" stroke="null"/>
+                </g>
+            </svg>
+        </div>
+        <div class="cNlNr">
+            <h2>Circle,Line and Rectangle</h2>
+                <svg width="200" height="300" style="background-color: #00fa9a;">
+                    <g>
+                    <line x1="0" y1="75" x2="200" y2="75" stroke-width="5" stroke="#fff85b"/>
+                    <line x1="0" y1="225" x2="200" y2="225" stroke-width="5" stroke="#fff85b"/>
+                    <line x1="0" y1="150" x2="200" y2="150" stroke-width="5" stroke="#fff85b"/>
+                    <rect x="50" y="50" fill="#fff85b" height="50" width="100"/>
+                    <rect x="50" y="125" fill="#fff85b" height="50" width="100"/>
+                    <rect x="50" y="200" fill="#fff85b" height="50" width="100"/>
+                    </g>
+                    <g>
+                    <circle cx="100" cy="151" r="20" fill="#00fa9a" stroke="null" stroke-width="1.5"/>
+                    <circle cx="100" cy="76" r="20" fill="#00fa9a" stroke="null" stroke-width="1.5"/>
+                    <circle cx="100" cy="226" r="20" fill="#00fa9a" stroke="null" stroke-width="1.5"/>
+                    </g>
+                    <g>
+                    <line x1="70" y1="0" x2="70" y2="300" stroke-width="5" stroke="#00fa9a"/>
+                    <line x1="100" y1="0" x2="100" y2="300" stroke-width="5" stroke="#00fa9a"/>
+                    <line x1="130" y1="0" x2="130" y2="300" stroke-width="5" stroke="#00fa9a"/>
+                    </g>
+                </svg>
+        </div>
+        <div class="polys">
+            <h2>Polygon and Polyline</h2>
+                <svg width="200" height="300" style="background-color:#ff7256">
+                    <g>
+                    <polyline points="-5,40 40,10 80,40 120,10 160,40 205,10" style="fill:#ff7256;stroke:#fdb924;stroke-width:10" />
+                    <polygon points="10,50 40,30 70,50" fill="#fdb924" stroke-width="2" stroke="null"/>
+                    <polygon points="90,50 120,30 150,50" fill="#fdb924" stroke-width="2" stroke="null"/>
+                    </g>
+                    <g>
+                    <polyline points="-5,150 40,150 98,75 155,150 200,150" style="fill:#ff7256;stroke:#fdb924;stroke-width:3" />
+                    <polyline points="-5,150 40,150 98,220 155,150 200,150" style="fill:#ff7256;stroke:#fdb924;stroke-width:3" />
+                    <polygon points="55,150 95,95 95,200" fill="#fdb924" stroke-width="2" stroke="null"/>
+                    <polygon points="140,150 100,95 100,200" fill="#fdb924" stroke-width="2" stroke="null"/>
+                    
+                    </g>
+                    <g>
+                    <polyline points="-5,260 40,230 80,260 120,230 160,260 205,230" style="fill:#ff7256;stroke:#fdb924;stroke-width:10" />
+                    <polygon points="10,270 40,250 70,270" fill="#fdb924" stroke-width="2" stroke="null"/>
+                    <polygon points="90,270 120,250 150,270" fill="#fdb924" stroke-width="2" stroke="null"/>
+                    </g>
+                </svg>
+        </div>
+        <div class="all">
+            <h2>Circle,Rectangle,Polyline,Polygon and Ellipse</h2>
+            <svg width="200" height="300" style="background-color:#434c5e">
+                <g>
+                  <circle cx="40" cy="40" r="15" fill="#ffb90f" stroke="null" stroke-width="1.5"/>
+                  <circle cx="160" cy="40" r="15" fill="#ffb90f" stroke="null" stroke-width="1.5"/>
+                  <circle cx="155" cy="40" r="15" fill="#434c5e" stroke="null" stroke-width="1.5"/>
+                  <ellipse cx="130" cy="80" rx="25" ry="10" fill="white" stroke-width="1" stroke="null"/>
+                  <ellipse cx="150" cy="90" rx="25" ry="10" fill="white" stroke-width="1" stroke="null"/>
+                  <ellipse cx="110" cy="90" rx="25" ry="10" fill="white" stroke-width="1" stroke="null"/>
+                </g>
+                <g>
+                  <rect y="210" fill="#00bfff" height="90" width="200"/>
+                  <polygon points="0,210 65,100 120,210" fill="#868e96" stroke-width="2" stroke="null"/>
+                  <polygon points="65,210 130,150 200,210" fill="#adb5bd" stroke-width="2" stroke="null"/>
+                  <line x1="0" y1="120" x2="60" y2="120" stroke-width="2" stroke="#434c5e"/>
+                  <line x1="0" y1="135" x2="60" y2="135" stroke-width="2" stroke="#434c5e"/>
+                  <line x1="0" y1="150" x2="60" y2="150" stroke-width="2" stroke="#434c5e"/>
+                  <line x1="0" y1="165" x2="60" y2="165" stroke-width="2" stroke="#434c5e"/>
+                  <line x1="0" y1="180" x2="60" y2="180" stroke-width="2" stroke="#434c5e"/>
+                  <line x1="0" y1="195" x2="60" y2="195" stroke-width="2" stroke="#434c5e"/>
+                  <line x1="0" y1="210" x2="200" y2="210" stroke-width="10" stroke="#f0e68c"/>
+                  <polyline points="-5,290 20,270 50,290 80,270 110,290 140,270 170,290 200,270" style="fill:#00bfff;stroke:white;stroke-width:5"/>
+                  <polyline points="-5,250 20,230 50,250 80,230 110,250 140,230 170,250 200,230" style="fill:#00bfff;stroke:white;stroke-width:5"/>
+                </g>
+              </svg>
+        </div>
+    </body>
+</html>
